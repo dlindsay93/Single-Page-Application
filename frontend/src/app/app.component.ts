bc9a416c8ccbabf6638e50b681b3185a5930e4d7
dlindsay93 <davidlindsay874@gmail.com>
2019-01-22 14:55:57 -0600

    fixed front end to display rating

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
-  userRating: string;
-  displayRating: string = "This rating was";
+export class AppComponent implements OnInit {
 
-  inputRating(){
-    switch(this.userRating) {
-        case 'BAD':
-          return this.userRating === 'BAD';
-          this.displayRating = "This rating was BAD";
-      }
-    }
+  constructor() { }
+
+  ngOnInit() {}
+
+  userRating: string;
+  showRating: string = "";
+
+  // ratingClass(){
+  //   switch(this.userRating){
+  //       case 'TERRIBLE':
+  //         return this.userRating = 'TERRIBLE';
+  //       case 'VERY BAD':
+  //         return this.userRating = 'VERY BAD';
+  //       case 'BAD':
+  //         return this.userRating = 'BAD';
+  //       case 'PASSABLE':
+  //         return this.userRating = 'PASSABLE';
+  //       case 'GOOD':
+  //         return this.userRating = 'GOOD';
+  //       case 'AMAZING':
+  //         return this.userRating = 'AMAZING';
+  //
+  //     }
+  //   }
+  updatedRating(rating: string){
+    return console.log(rating);
+    this.showRating = rating;
+  }
 }
