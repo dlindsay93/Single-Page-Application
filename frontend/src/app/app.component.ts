bf8b36c6465d2a00007a1481f5d6d74aac76ee9c
dlindsay93 <davidlindsay874@gmail.com>
2019-01-21 21:39:19 -0600

    worked on frontend to have rating system

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  userRating: string;
+  displayRating: string = "This rating was";
+
+  inputRating(){
+    switch(this.userRating) {
+        case 'BAD':
+          return this.userRating === 'BAD';
+          this.displayRating = "This rating was BAD";
+      }
+    }
 }
